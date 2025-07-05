0e1953a83eeb397e882ba7e48cc5c82fbd491241
Narthana Epa <narthana.epa@gmail.com>
2025-07-06 09:29:09 +1000

    Parse in main

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 mod parser;
 
-fn main() {
-    println!("Hello, world!");
+use color_eyre::Result;
+
+fn main() -> Result<()> {
+    let codeowner = std::fs::read_to_string(".github/CODEOWNERS")?;
+    let parsed = parser::parse(&codeowner);
+
+    println!("{parsed:#?}");
+
+    Ok(())
 }

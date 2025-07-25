96d2937ed1e84245c362e898b671c555b8bbf8e5
Narthana Epa <narthana.epa@gmail.com>
2025-07-25 21:40:40 +1000

    Parse args with clap

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,33 @@
 mod owners;
 mod parser;
 
+use clap::Parser;
 use color_eyre::Result;
 use ignore::gitignore::GitignoreBuilder;
 use std::{collections::HashMap, io::stdin};
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Path to the CODEOWNERS file
+    #[arg(short, long, default_value = ".github/CODEOWNERS")]
+    path: String,
+}
+
 fn main() -> Result<()> {
-    let codeowner_path = ".github/CODEOWNERS";
-    let cwd = std::env::current_dir()?;
-    let codeowner = std::fs::read_to_string(codeowner_path)?;
+    let args = Args::parse();
 
-    let owners = parser::parse(&codeowner);
+    let codeowner = std::fs::read_to_string(args.path)?;
 
-    let owners_by_glob = owners
+    let codeowners = parser::parse(&codeowner);
+    let codeowners_by_glob = codeowners
         .clone()
         .into_iter()
         .map(|owned_path| (owned_path.path.clone(), owned_path.owners))
         .collect::<HashMap<String, Vec<String>>>();
 
-    let mut builder = GitignoreBuilder::new(cwd);
-    let builder = owners
+    let mut builder = GitignoreBuilder::new(std::env::current_dir()?);
+    let builder = codeowners
         .into_iter()
         .try_fold(&mut builder, |acc, owned_path| {
             acc.add_line(None, &owned_path.path)
@@ -33,7 +41,7 @@ fn main() -> Result<()> {
         Some(line.to_string())
     });
 
-    owners::find(builder.build()?, owners_by_glob, paths).for_each(|(path, owners)| {
+    owners::find(builder.build()?, codeowners_by_glob, paths).for_each(|(path, owners)| {
         if !owners.is_empty() {
             println!("{path}: {}", owners.join(", "));
         } else {
